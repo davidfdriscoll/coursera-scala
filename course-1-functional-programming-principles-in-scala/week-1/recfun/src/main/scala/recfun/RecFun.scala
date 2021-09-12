6ac89b35a1137c9e90d741dd84fe9db930d58977
davidfdriscoll <david.f.driscoll@gmail.com>
2021-09-11 21:27:47 -0400

    Week-1 RecFun solutions

--- course-1-functional-programming-principles-in-scala/week-1/recfun/src/main/scala/recfun/RecFun.scala
+++ course-1-functional-programming-principles-in-scala/week-1/recfun/src/main/scala/recfun/RecFun.scala
@@ -12,14 +12,38 @@ object RecFun extends RecFunInterface:
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = ???
-
+  def pascal(c: Int, r: Int): Int = {
+    if(c == r || c == 0) 1
+    else pascal(c, r-1) + pascal(c-1, r-1)
+  }
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+    def charsIter(chars: List[Char], parCount: Int): Boolean = {
+      if(parCount < 0) return false
+      if(chars.isEmpty) return parCount == 0
+      val newCount = 
+        if(chars.head == '(') parCount + 1
+        else if (chars.head == ')') parCount - 1
+        else parCount
+      charsIter(chars.tail, newCount)
+    }
+
+    charsIter(chars, 0)
+  }
 
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  // In each particular case the first coin is either part of the solution or it isn't
+  // if it is part of the solution, we use it and keep it to use on the smaller amount
+  // if it is not, we remove it from the set of possible coins and consider the money
+  // against the remaining set.
+  // Continue until we hit our base cases (money is 0 or negative, or we have no more coins)
+  def countChange(money: Int, coins: List[Int]): Int = {
+    if(money == 0) 1
+    else if(money < 0) 0
+    else if(coins.isEmpty) 0
+    else countChange(money, coins.tail) + countChange(money - coins.head, coins)
+  }
